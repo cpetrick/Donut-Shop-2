a8eb811dfb01afd1a2bc7cfb3c5356247d84982f
Chris Petrick <r.chris.petrick@gmail.com>
2015-08-17 18:31:24 -0700

    Console Log Commit

---
+++ DonutShop.js
@@ -0,0 +1,33 @@
+
+
+<script>
+
+var shop = {
+	name: 'East Side',
+	minCustHour: '8',
+	maxCustHour: '43',
+	avgDonPerCust: '4.5',
+
+	donutsPerHour: function( {
+		return this.
+	})
+}
+
+
+
+
+
+
+</script>
+
+
+//  1. A method to calculate and display the number of donuts each 
+// 	shop needs to bake per hour and per day.
+
+//  2. The customers per hour should be randomly generated (Links to 
+// 	an external site.) from the range provided for every hour of the day.
+
+//  3. The output should be sent to the console log.
+
+//  4. Using the constructor function, create new objects to represent 
+// 	each location and simulate a days worth of baking.
